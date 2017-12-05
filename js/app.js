170b3093186b7c2c72152b075b53622853727349
Chike Ezeh <ezeh.chike@gmail.com>
2017-12-05 07:13:27 -0600

    add location model data

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
-var appModel = function(){
+// data model for locations to show on map
+var locations = [
+    {title: 'Lebanon\'s Cafe', location: {lat: 29.949681, lng: -90.127463}},
+    {title: 'The Halal Guys', location: {lat: 29.935162, lng: --90.108591}},
+    {title: 'Felipe\'s Mexican Taqueria', location: {lat: 29.946726, lng: -90.112940}},
+    {title: 'Commander\'s Palace', location: {lat: 29.928739, lng: -90.084253}},
+    {title: 'The Bulldog, Uptown', location: {lat: 29.923453, lng: -90.087374}}
+];
+var ViewModel = function(){
     // define our model
 
 
@@ -14,4 +22,4 @@ function initMap() {
     zoom: 13
     });
 }
-ko.applyBindings(new appModel());
+ko.applyBindings(new ViewModel());
